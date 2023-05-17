1bd1ceef99cad3e950896d62bd9dfeab19ca6bf2
kellims <kellisimon18@gmail.com>
2023-05-16 19:29:36 -0600

    added information controller

---
+++ Back_End/controllers/information.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+const { Information } = require('../models');
+
+router.get('', async (req, res, next) => {
+    try {
+        const myInformation = await Information.find({});
+            console.log(myInformation);
+            res.json(myInformation);
+        
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+});
+
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const theInformation = await Information.findById(req.params.id);
+        console.log(theInformation);
+        res.json(theInformation)
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+});
+
+router.post('', async (req, res, next) => {
+    try {
+        const newInformation = req.body
+        await Information.create(req.body);
+        console.log(newInformation);
+        res.redirect('/')
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+});
+
+module.exports = router;
