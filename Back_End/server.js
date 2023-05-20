b2def21b85aecb0551497e0572160c8269d21449
kellims <kellisimon18@gmail.com>
2023-05-20 08:28:07 -0600

    updated package.json

--- Back_End/server.js
+++ Back_End/server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -8,6 +9,7 @@ const locationsController = require('./controllers/locations')
 const informationController = require('./controllers/information')
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json("Hello World")
